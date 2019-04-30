805c4ec965dba728da7c20a1835cb480ac753429
PierreLePage98 <50101429+PierreLePage98@users.noreply.github.com>
2019-04-30 16:32:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home.js";
 import Login from "./pages/login.js";
 import SignUp from "./pages/signup.js";
+import Categories from "./pages/Categories.js";
+import VentesFlash from "./pages/ventesflash.js";
+import Vendre from "./pages/vendre.js";
+import VotreCompte from "./pages/votrecompte.js";
+import Panier from "./pages/panier.js";
+import Admin from "./pages/admin.js";
 
 function App() {
   return (
@@ -14,6 +20,13 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/Categories" component={Categories} />
+          <Route path="/ventesflash" component={VentesFlash} />
+          <Route path="/vendre" component={Vendre} />
+          <Route path="/votrecompte" component={VotreCompte} />
+          <Route path="/panier" component={Panier} />
+          <Route path="/admin" component={Admin} />
+
         </div>
       </Router>
     </div>
