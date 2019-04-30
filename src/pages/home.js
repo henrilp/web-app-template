0304d02a38f5b38ffb256dc06d7485fd1404773c
henrilp <henrilepage@hotmail.fr>
2019-04-30 11:54:29 +0200

    travail sur le layout (la structure des pages)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PublicMenu from '../components/menus/publicMenu.js'
 import Header from '../components/banners/header1.js'
+import {Segment,Image} from "semantic-ui-react"
 
 export default class Home extends Component {
 
@@ -17,8 +18,11 @@ export default class Home extends Component {
       <div>
         <Header title="ECE : E-Commerce Elégant"/>
         <PublicMenu location={this.props.location}/>
-        <hr/>
-        <p>Home</p>
+        <div style={{padding:'4rem'}}>
+          <Segment loading>
+            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+          </Segment>
+        </div>
       </div>
     )
 	}
