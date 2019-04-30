315f898a1bed9fafec2ed2eee6ffb1ac42c57445
Pierre LE PAGE <pierre.le-page@edu.ece.fr>
2019-04-30 16:10:49 +0200

    Menu avancé

---
+++ src/pages/panier.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PublicMenu from '../components/menus/publicMenu.js'
+import Header from '../components/banners/header1.js'
+import {Grid,Segment,Image} from "semantic-ui-react"
+
+export default class Panier extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillMount() {
+  }
+
+  render(){
+    return(
+      <div>
+        <Header title="Payement"/>
+        <PublicMenu location={this.props.location}/>
+
+        <Grid columns="equal" style={{padding:"2rem",paddingLeft:'10rem',paddingRight:'10rem'}}>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment loading color="green">
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment loading color="blue">
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment loading color="red">
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
+	}
+}
